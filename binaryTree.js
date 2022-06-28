5a6473b115922ca1d2244cda9547d300f3c6e32f
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-28 12:27:45 -0700

    very close to a solution

--- binaryTree.js
+++ binaryTree.js
@@ -5,15 +5,56 @@ class BinaryTreeNode {
     this.right = null;
   }
   add(node) {
+    if (node < this.value && this.left) {
+      this.left = new BinaryTreeNode(this.left);
+      this.left.add(node);
+    }
+    if (node < this.value && !this.left) {
+      this.left = node;
+    }
+    if (node > this.value && this.right) {
+      this.right = new BinaryTreeNode(node);
+    }
+    if (node > this.value && !this.right) {
+      this.right = node;
+    }
     // if node is less than value go to the left
     // if node is greater than value
-    // if nothing there then this.left = value 
-    this.left = new BinaryTreeNode(node);
+    // if nothing there then this.left = value
   }
 }
-  
+
+//BLESS IT I WAS SO CLOSE!!!!
+
+//Bradley's solution
+// class BinaryTreeNode {
+//   constructor(value) {
+//       this.value = value;
+//       this.left = null;
+//       this.right = null;
+//   }
+
+//   add(node) {
+//       if (node.value < this.value) {
+//           if (!this.left) {
+//               this.left = node;
+//           } else {
+//               this.left.add(node);
+//           }
+//       } else {
+//           if (!this.right) {
+//               this.right = node;
+//           } else {
+//               this.right.add(node);
+//           }
+//       }
+//   }
+// }
+
 class BinaryTree {
   constructor(value) {
     this.root = new BinaryTreeNode(value);
   }
 }
+
+module.exports = { BinaryTree, BinaryTreeNode };
--- index.test.js
+++ index.test.js
@@ -4,11 +4,11 @@ describe("LinkedListNode class", () => {
   it("should add a node at the end of our LinkedList", () => {
     const list = new LinkedList("A");
     list.add("B");
-    console.log(list.getList());
+    // console.log(list.getList());
     expect(list.getList().length).toBe(2);
     list.add("C");
     expect(list.getList().length).toBe(3);
-  })
+  });
 
   it("should remove the last node from our LinkedList", () => {
     const list = new LinkedList("A");
